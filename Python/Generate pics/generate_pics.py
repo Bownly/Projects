658172881767da8f820f37c08a5c1e6214f1ba6d
bownly <kopec@mail.usf.edu>
2016-08-18 17:22:50 -0400

    Upload
    Description

---
+++ Python/Generate pics/generate_pics.py
@@ -0,0 +1,108 @@
+from array import *
+from PIL import Image
+import math
+import os
+
+
+xdim = 5
+ydim = 5
+
+# colors = [  (0, 0, 255), 
+# 			(0, 255, 0),
+# 			(255, 0, 0),
+# 			(255, 255, 0),
+# 			(255, 0, 255),
+# 			(0, 255, 255),]
+
+colors = [  (0,   0,   0), 
+			# (128, 128, 128), 
+			(255, 255, 0),]
+
+def color_square(shade, index, arr):
+
+	global total
+	# end condition
+	if index > xdim*ydim:
+		if total%2 == 0:
+			img = Image.new('RGB', (xdim, ydim), "black") # create a new black image
+			pixels = img.load() # create the pixel map
+			for x in range(img.size[0]):    # for every pixel:
+			    for y in range(img.size[1]):
+			        pixels[img.size[0]-x-1,img.size[1]-y-1] = (arr[y][x]) # set the colour accordingly
+
+			# save image file
+			filename = "genpics/" + str(total/20000) + "/" + str(total/2) + ".png"
+			if not os.path.exists(os.path.dirname(filename)):
+			    try:
+			        os.makedirs(os.path.dirname(filename))
+			    except OSError as exc: # Guard against race condition
+			        if exc.errno != errno.EEXIST:
+			            raise
+			img.save(filename, "PNG")
+		total += 1
+		return
+
+	# establish coords
+	y = int(math.ceil(float(index / float(xdim))) - 1)
+	x = int(index - y * xdim) - 1
+
+	# color the square
+	arr[y][x] = shade
+
+	# recurse
+	for color in colors:
+		b = [[arr[x][y] for y in range(len(arr[0]))] for x in range(len(arr))]
+		color_square(color, index+1, b)
+
+
+def decode_string(x, y, seed):
+
+	global total
+	img = Image.new('RGB', (x, y), "black") # create a new black image
+	pixels = img.load() # create the pixel map
+	for i in range(x):
+		for j in range(y):
+			u = x - i - 1
+			v = y - j - 1
+			# pow_num = math.pow(2, u*x+v)
+			pow_num = math.pow(2, u*y+v)
+			# print u, v, "<- u v, pow_num->", int(pow_num)
+			if int(pow_num) <= seed:
+				seed -= pow_num
+				pixels[v, u] = (0, 255, 255)
+	filename = "genpics/" + str(int(total)/20000) + "_decode/" + str(int(total)) + ".png"
+	if not os.path.exists(os.path.dirname(filename)):
+		    try:
+		        os.makedirs(os.path.dirname(filename))
+		    except OSError as exc: # Guard against race condition
+		        if exc.errno != errno.EEXIST:
+		            raise
+	img.save(filename, "PNG")
+	# img.show()
+	total += 1
+
+
+
+# total = 0
+# decode_string(xdim, ydim, 175786)
+
+# 24804541 (lol)
+
+total = 0
+reinj = int(math.pow(2, xdim*ydim))
+for seed in range(int(reinj*0.5), int(reinj*.75)):
+	decode_string(xdim, ydim, seed)
+
+# total = 0
+# lst = [1] * xdim
+# lstlst = [lst] * ydim
+# for color in colors:
+# 	color_square(color, 1, lstlst)
+
+
+
+
+
+
+
+
